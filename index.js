b3bc74bba5d3a0107bd58ae38558b59a019ae7f3
Edward Yeh <edward_yeh@gemteks.com>
2017-11-10 15:44:29 +0800

    Show status code and message from the response.

--- index.js
+++ index.js
@@ -2,19 +2,33 @@ const request = require('request-promise');
 
 const options = {
     method: 'POST',
-    uri: 'http://gocd:1706f187577b6cb1efb68b22c32938a9@10.70.51.243:8080/job/DropAP_Android/build',
-    qs: {
-        token: 'TRIGGER_BY_GOCD'
+    uri: 'http://10.70.51.243:8080/job/DropAP_Android/build',
+    auth: {
+        user: 'gocd',
+        pass: '1706f187577b6cb1efb68b22c32938a9'
     },
     headers: {
         'Jenkins-Crumb': '3f33aa35e77cbc14bd3f91ccd6de742b'
-    }
+    },
+    qs: {
+        token: 'TRIGGER_BY_GOCD'
+    },
+    resolveWithFullResponse: true,
+    json: true
 };
 
 request(options)
     .then(function (response) {
-        console.log('Success');
+        showStatusOf(response);
     })
     .catch(function (err) {
-        console.log(err);
+        console.log('[ERROR] Fail to trigger Build task');
+        showStatusOf(err.response);
+        process.exit(1);
     });
+
+function showStatusOf(response) {
+    if (!response) return;
+    console.log('status code: ' + response.statusCode);
+    console.log('status message: ' + response.statusMessage);
+}
